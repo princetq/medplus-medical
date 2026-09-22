f54389b9d62d3ffef2f6821d97bdbb86337eeaa4
princetq <princetq@gmail.com>
2026-09-22 15:32:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// MEDPLUS R10 CONFIG - chỉnh đúng URL Apps Script sau khi deploy.
+window.MEDPLUS_AUTH_BACKEND_URL='https://script.google.com/macros/s/AKfycbzc3-RgmbSEmV59vn4lxDoiFDRK_jq1wdeTN6QN12_ekBkRlFdbQVdDdFTt1F8U_IFcRg/exec';
+// Có thể để đường dẫn tương đối nếu JSON nằm cùng repository GitHub Pages.
+window.MEDPLUS_DB_JSON_URL='./data/medplus-database.json';
